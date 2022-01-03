26f233ea808ae90bab0ba23c96ba32345f4a7dc2
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-03 11:39:03 -0300

    first app switched to our name and explanations regarding index, approot...and so on

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//se olharmos nosso index.html ,isso aqui se torna uma "tag nossa", pois na linha 11 do index ele referencia a isso aq
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Roberto';
 }
