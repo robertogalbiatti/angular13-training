0cacc4e83af92353c8a50e5c7e982db0b9a179b9
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-06 10:38:04 -0300

    changing our style from css file from component style, direct from app.component.ts file

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',//se olharmos nosso index.html ,isso aqui se torna uma "tag nossa", pois na linha 11 do index ele referencia a isso aq
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+     color: dodgerblue
+    }
+    `]
 })
 export class AppComponent {
 }
