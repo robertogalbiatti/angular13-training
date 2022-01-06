dad4de95eb49f91a47dec37f0bd8bf55d26ed6ba
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-06 18:15:25 -0300

    using Event biding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -25,5 +26,9 @@ export class ServersComponent implements OnInit {
   //usando "on" significa que será gatilhado dentro do Template, será um evento chamado
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value; //utilizando casting para este caso
   }
 }
