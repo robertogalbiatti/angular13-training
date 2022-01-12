844a4932f10733eceaa0ff9212a3ee53ebb11525
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-12 16:45:13 -0300

    Created our ingredient model for our ingredient obj and displayed on screen our shopping list component with ingredients

---
+++ shopping-recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+    /*public name: string;
+    public amount: number;
+    constructor(name: string, amount: number) {
+        this.name = name;
+        this.amount = amount;
+    }*/
+    // Um atalho para trocar isso para algo mais "limpo"
+    constructor(public name: string,  public amount: number) {}
+}
