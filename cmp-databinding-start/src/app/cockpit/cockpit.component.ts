c96d78a9bfbc336144bb959d5e543f90da4688a0
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-24 10:54:58 -0300

    alias to blueprint in Cockpit component

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   //para lembrar: estamos passando algo para fora do componente, do filho para o pai (decorator @Output)
   //isso abaixo é uma propriedade em Angula, serverCreated é um evento, e evento possui essa estrutura no Angular
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();;
   newServerName = '';
   newServerContent = '';
 
