1b923e937cace22b339caab4630844c1516ec72e
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-26 15:20:11 -0300

    Last learning regarding hooks, ngOnInit and so on...

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,51 +1,87 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-server-element',
-  templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated //None or Native
+  selector: "app-server-element",
+  templateUrl: "./server-element.component.html",
+  styleUrls: ["./server-element.component.css"],
+  encapsulation: ViewEncapsulation.Emulated, //None or Native
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   //decorator e normalmente tem essa cara de função
   //dentro dos parenteses podemos usar um 'alias'
-  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input("srvElement") element: { type: string; name: string; content: string };
   @Input() name: string;
+  @ViewChild("heading", { static: true }) header: ElementRef;
+  @ContentChild("contentParagraph", { static: true }) paragraph: ElementRef;
 
   constructor() {
-    console.log('constructor called')
+    console.log("constructor called");
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnInit called')
-    console.log(changes)
+    console.log("ngOnInit called");
+    console.log(changes);
   }
 
   ngOnInit() {
-    console.log('ngOnInit called')
+    console.log("ngOnInit called");
+    console.log("Text Content: " + this.header.nativeElement.textContent);
+    console.log(
+      "Text Content of paragraph: " + this.paragraph.nativeElement.textContent
+    );
   }
 
   ngDoCheck() {
-    console.log('ngDoCheck called')
+    console.log("ngDoCheck called");
   }
 
   ngAfterContentInit() {
-    console.log('ngAfterContentInit called')
+    console.log("ngAfterContentInit called");
+    console.log(
+      "Text Content of paragraph: " + this.paragraph.nativeElement.textContent
+    );
   }
 
   ngAfterContentChecked() {
-    console.log('ngAfterContentChecked called')
+    console.log("ngAfterContentChecked called");
   }
 
   ngAfterViewInit() {
-    console.log('ngAfterViewInit called')
+    console.log("ngAfterViewInit called");
+    console.log("Text Content: " + this.header.nativeElement.textContent);
   }
 
   ngAfterViewChecked() {
-    console.log('ngAfterViewChecked called')
+    console.log("ngAfterViewChecked called");
   }
-  
+
   ngOnDestroy() {
-  console.log('ngOnDestroy called')
+    console.log("ngOnDestroy called");
   }
 }
