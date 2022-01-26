74fbdae4fb76103f09800969dbdbaaa3472ecca9
GRO8CA <roberto.galbiattifilho@br.bosch.com>
2022-01-26 14:48:20 -0300

    Lyfecycles on practice

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,46 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None or Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
   //decorator e normalmente tem essa cara de função
   //dentro dos parenteses podemos usar um 'alias'
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called')
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnInit called')
+    console.log(changes)
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called')
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called')
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called')
+  }
+  
+  ngOnDestroy() {
+  console.log('ngOnDestroy called')
+  }
 }
